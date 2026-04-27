d0c8a92f98400f6d212bfcc909e2266c7bfffd3c
Rajesh Mohan <rittiksingh2814@gmail.com>
2026-04-27 18:36:03 +0530

    Even UID Questions done

---
+++ EST Practical/my-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", background: "#eee" }}>
+      <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+      <Link to="/about" style={{ marginRight: "10px" }}>About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
